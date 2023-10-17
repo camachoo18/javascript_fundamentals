fd6b60cd58731244258485f16461a4ae3f692d1e
Estiven <estivencamachops@gmail.com>
2023-10-17 19:56:49 +0200

    utilizacion de propiedades de objeto en texto

---
+++ 03_ejercicio.js
@@ -0,0 +1,10 @@
+let usuario = {
+    nombre: "Estiven",
+    edad: 21,
+    ciudad: "Granada"
+  };
+  
+  var saludo = "Hola me llamo " + usuario.nombre + ", tengo " + usuario.edad + " años y soy de " + usuario.ciudad;
+  
+  console.log(saludo);
+  
