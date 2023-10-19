be11ae2a32fa04dfff5803646ed090ed6634000b
Estiven <estivencamachops@gmail.com>
2023-10-19 19:52:47 +0200

    Mostrar resultado operaciones

---
+++ 04_ejercicio.js
@@ -0,0 +1,43 @@
+function suma(a,b){
+    let resultado = a + b
+    return resultado
+}
+
+
+suma(2,6)
+
+
+function restar(a,b){
+    let resultado = a - b
+    return resultado
+}
+
+
+restar(8,2)
+
+
+function multiplicar(a,b){
+    let resultado = a * b
+    return resultado
+}
+
+
+multiplicar(3,2)
+
+
+function dividir(a,b){
+    let resultado = a / b
+    return resultado
+}
+
+
+dividir(8,4)
+
+
+function doble (a){
+    let resultado = a * 2
+    return resultado
+}
+
+
+doble(2)
