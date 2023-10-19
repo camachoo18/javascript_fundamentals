8ff87a8fcf608d2447208f0b95ad1ee63d2cc505
Estiven <estivencamachops@gmail.com>
2023-10-19 20:52:49 +0200

    calcular area circulo

---
+++ 05_ejercicio.js
@@ -0,0 +1,6 @@
+function areaCirculo(r){
+    let resultado = Math.PI * Math.sqrt(r) 
+    return "El resultado del area del circulo es: " + resultado
+}
+var operacion = areaCirculo(4)
+console.log (operacion)
