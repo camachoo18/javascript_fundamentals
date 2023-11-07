27d1ee29c70dcd9c4d48d8c74ee9fb101cc98a41
Estiven <estivencamachops@gmail.com>
2023-11-07 23:23:53 +0100

    ejercicio #18

---
+++ ejercicio_18.js
@@ -0,0 +1,10 @@
+const frase_original = "Hola mundo";
+
+function cambioVocales(string, vocal){
+    let nueva_frase = "hala manda";
+
+    return nueva_frase;
+}
+
+const frase_modificada = cambioVocales(frase_original, "a"); //esto deberia devolver "Hala manda"
+console.log(frase_modificada)
